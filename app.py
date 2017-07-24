fd1d6e1e7738f4f472a8e97050eeca6ab1aa99d1
roaa wadi <roaa2wadi@gmail.com>
2017-07-24 10:58:56 +0300

    added route

--- app.py
+++ app.py
@@ -10,6 +10,31 @@ app = Flask(__name__)
 def homepage():
 	return render_template("homepage.html")
 
+@app.route("/aboutus")
+def aboutus():
+	return render_template("aboutus.html")
+
+@app.route("/contactus")
+def contactus():
+	return render_template("contactus.html")
+
+@app.route("/south")
+def southpage():
+	return render_template("south.html")
+
+@app.route("/north")
+def northpage():
+	return render_template("north.html")
+
+@app.route("/east")
+def eastpage():
+	return render_template("east.html")
+
+@app.route("/west")
+def westpage():
+	return render_template("west.html")
+
+
 
 
 if (__name__)==("__main__"):
