0fde8c5ed71ae89cc551f796a78a5046e7c0d1ed
khaled zaqout <zaqout.khaled@gmail.com>
2017-07-24 15:10:54 +0300

    added forms and veiw them

--- app.py
+++ app.py
@@ -34,7 +34,26 @@ def eastpage():
 def westpage():
 	return render_template("west.html")
 
+@app.route("/contactus")
+def contact_us():
+	return render_template("contactus.html")
 
+
+@app.route("/viewtheuser", methods= ["POST"])
+def hello():
+	user_name = request.form["firstname"]
+	user_lastname = request.form["lastname"]
+	user_message = request.form["message"]
+	user_gender = request.form["gender"]
+	# info_table.insert(dict(firname="user_name" , lasname="user_lastname", message="user_message" , gender= "user_info"))
+	# return info_table.find_one(firname="khaled")
+	
+	return render_template("veiwtheuser.html" ,
+		username= user_name,
+		userlastname=user_lastname,
+		usermessage=user_message,
+		usergender=user_gender
+		) 
 
 
 if (__name__)==("__main__"):
